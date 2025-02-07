cdd4395675681fa873c936d26d1aee41654eb687
Irina <irina.bojkovska.ib@gmail.com>
2025-02-07 05:59:43 -0800

    Fixed deployment issue

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/Irina11122/Final-React-Project.git',
+  base: '/https://github.com/Irina11122/Final-React-Project.git/',
 });
